aaf7f1990a47a3340f4d69b06eaefe53ac028d30
Davi <davispindola2828@gmail.com>
2024-05-27 17:16:45 +0000

    add

--- src/tool/app.tsx
+++ src/tool/app.tsx
@@ -1,12 +1,10 @@
 import { useReplit } from "@replit/extensions-react";
-import Index from "./routes";
 
 function App() {
   const { replit } = useReplit();
   const currentTheme = replit.themes.getCurrentThemeValues();
   return (
     <>
-      <Index />
       <pre>{JSON.stringify(currentTheme)}</pre>
     </>
   );
--- src/tool/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { useReplit } from "@replit/extensions-react";
-
-function Index() {
-	const { replit } = useReplit();
-	const out = replit.exec.exec("bun -v");
-	return <p>{out}</p>;
-}
-
-export default Index;
